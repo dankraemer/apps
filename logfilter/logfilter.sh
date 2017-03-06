1a20b1b9935554b57d8a357129b741eb6871466d
Daniel Kraemer Aliaga <daniel.aliaga@ge.com>
2017-03-06 16:00:14 -0300

    Handle missing sample message by source

--- logfilter/logfilter.sh
+++ logfilter/logfilter.sh
@@ -1,11 +1,38 @@
-#!/bin/bash
+#!/bin/sh
 
 if [ $# -eq 0 ]; then
 	echo $SCRIPT_MSG"Usage: $ ./logfilter.sh [MEASURES|ANALOG|DIGITAL] [Messages_file]"
 	exit 1
 fi
 
-echo $1
-echo $2
+TYPE=$1
 
-#grep 'MEASURES' messages | sed 's/  / /g' | cut -d ' ' -f 1-3,7,13,15 | sed 's/,//g' | sed 's/).//g' | awk '{print $4 " " $1 " " $2 " " $3 "\t" $5 "\t" $6 "\t" $5-$6}'
+INPUT=`cat $2 | grep "\[$TYPE\] Missing"`
+INPUT=`echo "$INPUT" | sed 's/  / /g' | sed 's/,//g' | sed 's/).//g'`
+
+case "$TYPE" in
+    "MEASURES")
+        SELECTED_FIELDS='1-3,7,13,15'
+        FIELDS=`echo "$INPUT" | cut -d ' ' -f $SELECTED_FIELDS`
+        FORMATED=`echo "$FIELDS" | awk '{print $4 " " $1 " " $2 " " $3 "\t" $5 "\t" $6 "\t" $5-$6}'`
+        ;;
+    "ANALOG")
+        SELECTED_FIELDS='1-3,7,12,14'
+        INPUT=`echo "$INPUT" | sed 's/)//g'`
+        FIELDS=`echo "$INPUT" | cut -d ' ' -f $SELECTED_FIELDS`
+        FORMATED=`echo "$FIELDS" | awk '{print $4 " " $1 " " $2 " " $3 "\t" $5 "\t" $6 "\t" $5-$6 "\t" ($5-$6)/128}'`
+        ;;
+    "DIGITAL")
+        SELECTED_FIELDS='1-3,7,12,14'
+        INPUT=`echo "$INPUT" | sed 's/)//g'`
+        FIELDS=`echo "$INPUT" | cut -d ' ' -f $SELECTED_FIELDS`
+        FORMATED=`echo "$FIELDS" | awk '{print $4 " " $1 " " $2 " " $3 "\t" $5 "\t" $6 "\t" $5-$6 "\t" ($5-$6)/128}'`
+        ;;
+    *)
+        exit 1
+        ;;
+esac
+
+OUTPUT=$FORMATED
+echo "$OUTPUT"
+
